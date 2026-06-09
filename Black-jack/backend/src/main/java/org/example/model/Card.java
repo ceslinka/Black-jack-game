5bb40fac78c354a668c45e9488dac15e82bc918e
rozalia <rozaliam@example.com>
2026-06-09 19:36:12 +0200

    ads

---
+++ Black-jack/backend/src/main/java/org/example/model/Card.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    public Rank getRank() {
+        return rank;
+    }
+    public Suit getSuit() {
+        return suit;
+    }
+    public int getValue() {
+        return rank.getValue();
+    }
+    @Override
+    public String toString() {
+        return "Card{" + "rank=" + rank + ", suit=" + suit + '}';
+    }
+}
---
+++ Black-jack/backend/src/main/java/org/example/model/Suit.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public enum Suit {
+    hearts,
+    diamonds,
+    spades,
+    clubs
+}
